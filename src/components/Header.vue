3843941828fa13b60807274ae35ff75d188a4794
anz <g_anz@mail.ru>
2021-10-03 23:25:54 +0300

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <header class="p-3 bg-dark text-white">
+      <div class="container">
+        <div
+          class="
+            d-flex
+            flex-wrap
+            align-items-center
+            justify-content-center justify-content-lg-start
+          "
+        >
+          <a
+            href="/"
+            class="
+              d-flex
+              align-items-center
+              mb-2 mb-lg-0
+              text-white text-decoration-none
+            "
+          >
+            <img
+              alt="Vue logo"
+              class="img-fluid logo"
+              src="../assets/logo.png"
+            />
+          </a>
+
+          <ul
+            class="
+              nav
+              col-12 col-lg-auto
+              me-lg-auto
+              mb-2
+              justify-content-center
+              mb-md-0
+            "
+          >
+            <router-link to="/" custom v-slot="{ href, navigate, isActive }">
+              <li>
+                <a
+                  :href="href"
+                  @click="navigate"
+                  :class="['nav-link px-2 text-white', isActive && 'text-white-50']"
+                  >Home</a
+                >
+              </li>
+            </router-link>
+
+            <router-link
+              to="about"
+              custom
+              v-slot="{ href, navigate, isActive }"
+            >
+              <li>
+                <a
+                  :href="href"
+                  @click="navigate"
+                  :class="['nav-link px-2 text-white', isActive && 'text-white-50']"
+                  >About</a
+                >
+              </li>
+            </router-link>
+            <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+          </ul>
+          <div class="text-end">
+            <button type="button" class="btn btn-outline-light me-2">
+              Login
+            </button>
+            <button type="button" class="btn btn-warning">Sign-up</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {},
+})
+export default class Header extends Vue {}
+</script>
+
+<style scoped lang="sass">
+.logo
+    width: 25px
+.active
+    color: grey
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Header />
+    <div class="container mt-5">
+      <h1 class="text-start">Upwork testing</h1>
+      
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Header from "@/components/Header.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    Header,
+  },
+})
+export default class Home extends Vue {}
+</script>
