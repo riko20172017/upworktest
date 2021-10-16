c26ea4bf7e00780868601fafd7e5869d5dc378f7
anz <g_anz@mail.ru>
2021-10-16 20:05:22 +0300

    add common styles

--- src/styles/index.vue
+++ src/styles/index.vue
@@ -4,4 +4,18 @@ $bg-classroom: #232323;
 // body {
 //   background: $bg-classroom;
 // }
+
+html,
+body,
+.app {
+  height: 100%;
+}
+.centered {
+  display: flex;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+  height: 100%;
+}
 </style>
